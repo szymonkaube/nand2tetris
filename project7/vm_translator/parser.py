42e68ef6fad06d0841c480088b93e37983031da8
szymon linux <szymon.kaube@gmail.com>
2026-03-05 22:05:02 +0100

    more or less finish parser

--- project7/vm_translator/parser.py
+++ project7/vm_translator/parser.py
@@ -1,15 +1,28 @@
+from data_types import Command, PushCommand, PopCommand, ArithmeticCommand
+
+
 class Parser:
     def __init__(self):
         self.comment_symbol = "//"
 
-    def parse(self, line: str):
+    def parse(self, line: str) -> Command:
         line = line.strip()
 
         if not line or line.startswith(self.comment_symbol):
             return None
 
         line = self._remove_inline_comment(line)
-        return line
+
+        parts = line.split()
+        cmd_type = parts[0]
+
+        match cmd_type:
+            case "push":
+                return PushCommand(parts[1], int(parts[2]))
+            case "pop":
+                return PopCommand(parts[1], int(parts[2]))
+            case _:
+                return ArithmeticCommand(cmd_type)
 
     def _remove_inline_comment(self, line: str) -> str:
         if self.comment_symbol in line:
