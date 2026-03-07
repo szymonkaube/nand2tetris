fb4fe78f47ded5fb299d884565006684b8c96301
szymon linux <szymon.kaube@gmail.com>
2026-03-07 20:26:20 +0100

    vm translator in usable state

--- project7/vm_translator/vm_translator.py
+++ project7/vm_translator/vm_translator.py
@@ -1,16 +1,22 @@
 import sys
 
 from parser import Parser
+from code import Code
 
 vm_code_filepath = sys.argv[1]
 with open(vm_code_filepath, 'r') as f:
     vm_lines = f.read().splitlines()
 
 parser = Parser()
-parsed_lines = []
+filename = vm_code_filepath.split("/")[0]
+code = Code(filename)
+
+asm_lines = []
 for line in vm_lines:
     parsed_line = parser.parse(line)
     if (parsed_line):
-        parsed_lines.append(parsed_line)
+        asm_code = code.get_assembly(parsed_line)
+        asm_lines.append(asm_code)
 
-print(parsed_lines)
+asm = "\n".join(asm_lines)
+print(asm)
