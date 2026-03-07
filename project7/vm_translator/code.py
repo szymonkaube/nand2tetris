c43a7953734108d3bba359350645693829fddf84
szymon linux <szymon.kaube@gmail.com>
2026-03-07 18:47:07 +0100

    rewrite pop method to use list

--- project7/vm_translator/code.py
+++ project7/vm_translator/code.py
@@ -25,38 +25,42 @@ class Code:
         segment_pointer = self.segment_map.get(push_command.segment)
         i = push_command.i
 
-        asm_load_d = ""
         if push_command.segment in ("local", "argument", "this", "that"):
-            asm_load_d = f"@{segment_pointer}\nD=M\n@{i}\nA=D+A\nD=M\n"
+            # addr = segment_pointer + i, D = *addr
+            asm = [f"@{segment_pointer}", "D=M", f"@{i}", "A=D+A", "D=M"]
         elif push_command.segment == "constant":
-            asm_load_d = f"@{i}\nD=A\n"
+            # D = i
+            asm = [f"@{i}", "D=A"]
         elif push_command.segment == "static":
-            asm_load_d = f"@{self.filename}.{i}\nD=M\n"
+            # D = *FileName.i
+            asm = [f"@{self.filename}.{i}", "D=M"]
         elif push_command.segment == "temp":
+            # D = *(5 + i)
             target_address = 5 + int(i)
-            asm_load_d = f"@{target_address}\nD=M\n"
+            asm = [f"@{target_address}", "D=M"]
         elif push_command.segment == "pointer":
+            # pointer 0 -> THIS, pointer 1 -> THAT
             segment_pointer = segment_pointer[i]
-            asm_load_d = f"@{segment_pointer}\nD=M\n"
+            asm = [f"@{segment_pointer}", "D=M"]
         else:
             raise ValueError(f"Push command has unknown segment type: {
                              push_command.segment}")
 
-        asm_push = (
-            "@SP\n"
-            "A=M\n"
-            "M=D\n"
-            "@SP\n"
-            "M=M+1\n"
-        )
+        # universal "push D to stack"
+        asm.extend([
+            "@SP",
+            "A=M",
+            "M=D",
+            "@SP",
+            "M=M+1"
+        ])
 
-        return asm_load_d + asm_push
+        return "\n".join(asm)
 
     def _get_pop_assembly(self, pop_command: PopCommand) -> str:
         segment_pointer = self.segment_map.get(pop_command.segment)
         i = pop_command.i
 
-        asm = []
         if pop_command.segment in ("local", "argument", "this", "that"):
             asm = [f"@{segment_pointer}", "D=M", f"@{i}", "D=D+A",
                    "@R13", "M=D",
